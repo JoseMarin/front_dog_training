02b2d9b2d4ee48cda895c89e8a322c327ca3f4a0
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-07-23 17:02:22 +0200

    Import header component && path of the Home

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Fragment } from 'react'
+
+import Home from './containers/Home/Home.jsx';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <BrowserRouter>
+        <Header />
+
+        <Switch>
+          <Route path="/" exact component={Home}/>
+        </Switch>
+
+      </BrowserRouter>
+    </Fragment>
   );
 }
 
