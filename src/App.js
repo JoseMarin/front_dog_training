94f607472dfe443016a09718d5cd4d38487ae29d
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-07-24 21:19:35 +0200

    Create path to login and sign up

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Fragment } from 'react'
 
 import Home from './containers/Home/Home.jsx';
-import Header from './components/Header/Header';
+import Header from './components/Header/Header.jsx';
+import Register from './containers/Register/Register';
+import Login from './containers/Login/Login.jsx'
+
 
 function App() {
   return (
@@ -13,6 +16,8 @@ function App() {
 
         <Switch>
           <Route path="/" exact component={Home}/>
+          <Route path="/register" exact component={Register}/>
+          <Route path="/login" exact component={Login}/>
         </Switch>
 
       </BrowserRouter>
