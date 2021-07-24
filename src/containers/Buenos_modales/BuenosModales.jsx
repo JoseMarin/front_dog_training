8193f233ed2d0f8c95d7d12926bd45400de1418e
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-07-24 22:06:06 +0200

    Create the containers to show the info that the application will offer by default

---
+++ src/containers/Buenos_modales/BuenosModales.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BuenosModales = () => {
+    return (
+        <div>
+            HELLO
+        </div>
+    )
+}
+
+export default BuenosModales;
---
+++ src/containers/Obediencia_basica/Obediencia_basica.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Obediencia_basica = () => {
+    return (
+        <div>
+            HELLO
+        </div>
+    )
+}
+
+export default Obediencia_basica;
