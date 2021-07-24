efac8ffa56b195ab21d8ea530a382aada10c5539
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-07-24 22:06:21 +0200

    Import all the containers

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import Home from './containers/Home/Home.jsx';
 import Header from './components/Header/Header.jsx';
 import Register from './containers/Register/Register';
 import Login from './containers/Login/Login.jsx'
+import ObedienciaBasica from './containers/Obediencia_basica/ObedienciaBasica.jsx'
+import BuenosModales from './containers/Buenos_modales/BuenosModales.jsx';
+import Ejercicios from './containers/Ejercicios/Ejercicios.jsx';
+import PerroNuevo from './containers/Perro_nuevo/PerroNuevo.jsx';
 
 
 function App() {
@@ -18,6 +22,10 @@ function App() {
           <Route path="/" exact component={Home}/>
           <Route path="/register" exact component={Register}/>
           <Route path="/login" exact component={Login}/>
+          <Route path="/obedienciabasica" exact component={ObedienciaBasica}/>
+          <Route path="/buenosmodales" exact component={BuenosModales}/>
+          <Route path="/ejercicios" exact component={Ejercicios}/>
+          <Route path="/perronuevo" exact component={PerroNuevo}/>
         </Switch>
 
       </BrowserRouter>
