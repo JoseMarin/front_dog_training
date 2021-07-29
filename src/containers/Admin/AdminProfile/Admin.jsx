3fcdb98ae30e98e9f08984ff90daea85d45beafd
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-07-29 12:26:54 +0200

    Add info to show

--- src/containers/Admin/AdminProfile/Admin.jsx
+++ src/containers/Admin/AdminProfile/Admin.jsx
@@ -39,16 +39,20 @@ const Admin = (props) => {
   } else {
     return (
 
-        <div class="card mb-3 rounded mx-auto d-block border- card" Style="max-width: 540px;">
+        <div class="card  mb-3 rounded mx-auto d-block border-0  card" Style="max-width: 540px;">
         <div class="row g-0">
           <div class="col-md-4">
             <img src="..." class="img-fluid rounded-start" alt="..."/>
           </div>
           <div class="col-md-8">
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+              <h5 class="card-title text-center">Welcome&nbsp; &nbsp; {props.credentials?.user.name}</h5>
+              <p class="card-text">EMAIL : {props.credentials?.user.email} </p>
+              <p class="card-text">{props.credentials?.user.city}</p>
+              <p class="card-text">
+              CREATECOUNT :{" "}
+              {moment(props.credentials?.user.createdAt).format("LL")}{" "}{/*Con 3 LLL te muestra la hora*/}
+            </p>
             </div>
           </div>
         </div>
