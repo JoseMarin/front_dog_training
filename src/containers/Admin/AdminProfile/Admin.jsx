335502ec0c7b4f2fb67a3c67fe493478c93b2e42
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-07-29 17:32:50 +0200

    Create profile admin

--- src/containers/Admin/AdminProfile/Admin.jsx
+++ src/containers/Admin/AdminProfile/Admin.jsx
@@ -4,7 +4,6 @@ import { connect } from "react-redux";
 import moment from "moment";
 
 const Admin = (props) => {
-
   if (props.credentials?.user.isAdmin === true) {
     return (
       <div className="admin">
@@ -16,11 +15,14 @@ const Admin = (props) => {
             <p>CITY : {props.credentials?.user.city}</p>
             <p>
               CREATECOUNT :{" "}
-              {moment(props.credentials?.user.createdAt).format("LL")}{" "}{/*Con 3 LLL te muestra la hora*/}
+              {moment(props.credentials?.user.createdAt).format("LL")}{" "}
+              {/*Con 3 LLL te muestra la hora*/}
             </p>
 
             <div className="box2">
-              <Link to={"/updateuser"} className="updateButton">UPDATE</Link>
+              <Link to={"/updateuser"} className="updateButton">
+                UPDATE
+              </Link>
             </div>
           </div>
         </div>
@@ -29,8 +31,12 @@ const Admin = (props) => {
           <div className="orderContent">
             <h2 className="titleUpdate">Access</h2>
             <div className="box2">
-                <Link className="updateButton" to={"/allorders"}>ORDERS</Link>
-                <Link className="updateButton" to={"/allusers"}>USERS </Link>
+              <Link className="updateButton" to={"/allorders"}>
+                ORDERS
+              </Link>
+              <Link className="updateButton" to={"/allusers"}>
+                USERS{" "}
+              </Link>
             </div>
           </div>
         </div>
@@ -38,27 +44,41 @@ const Admin = (props) => {
     );
   } else {
     return (
-
-        <div class="card  mb-3 rounded mx-auto d-block border-0  card" Style="max-width: 540px;">
+      <div
+        class="card  mb-3 rounded mx-auto d-block border-0  card"
+        Style="max-width: 540px;"
+      >
         <div class="row g-0">
           <div class="col-md-4">
-            <img src="..." class="img-fluid rounded-start" alt="..."/>
+            <img src="..." class="img-fluid rounded-start" alt="..." />
           </div>
           <div class="col-md-8">
             <div class="card-body">
-              <h5 class="card-title text-center">Welcome&nbsp; &nbsp; {props.credentials?.user.name}</h5>
+              <h5 class="card-title text-center">
+                Welcome&nbsp; &nbsp; {props.credentials?.user.name}
+              </h5>
               <p class="card-text">EMAIL : {props.credentials?.user.email} </p>
               <p class="card-text">{props.credentials?.user.city}</p>
               <p class="card-text">
-              CREATECOUNT :{" "}
-              {moment(props.credentials?.user.createdAt).format("LL")}{" "}{/*Con 3 LLL te muestra la hora*/}
-            </p>
+                CREATECOUNT :{" "}
+                {moment(props.credentials?.user.createdAt).format("LL")}{" "}
+                {/*Con 3 LLL te muestra la hora*/}
+              </p>
+              <div className="text-center">
+                <Link className="updateButton" to={"/allorders"}>
+                  ORDERS
+                </Link>
+                &nbsp; &nbsp;
+                <Link className="updateButton" to={"/allusers"}>
+                  USERS{" "}
+                </Link>
+              </div>
             </div>
           </div>
         </div>
       </div>
-
-    )}
+    );
+  }
 };
 
 export default connect((state) => ({
