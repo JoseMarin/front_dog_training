821820dca49b5ae04998998de58249ed7eaeda78
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-07-29 12:06:49 +0200

    Create admin file

---
+++ src/containers/Admin/AdminProfile/Admin.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import moment from "moment";
+
+const Admin = (props) => {
+
+  if (props.credentials?.user.isAdmin === true) {
+    return (
+      <div className="admin">
+        <div className="nombre"></div>
+        <div className="users">
+          <div className="userContent">
+            <h2>Welcome&nbsp; &nbsp; {props.credentials?.user.name}</h2>
+            <p>EMAIL : {props.credentials?.user.email} </p>
+            <p>CITY : {props.credentials?.user.city}</p>
+            <p>
+              CREATECOUNT :{" "}
+              {moment(props.credentials?.user.createdAt).format("LL")}{" "}{/*Con 3 LLL te muestra la hora*/}
+            </p>
+
+            <div className="box2">
+              <Link to={"/updateuser"} className="updateButton">UPDATE</Link>
+            </div>
+          </div>
+        </div>
+
+        <div className="orders">
+          <div className="orderContent">
+            <h2 className="titleUpdate">Access</h2>
+            <div className="box2">
+                <Link className="updateButton" to={"/allorders"}>ORDERS</Link>
+                <Link className="updateButton" to={"/allusers"}>USERS </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+
+        <div class="card mb-3 rounded mx-auto d-block border- card" Style="max-width: 540px;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="..." class="img-fluid rounded-start" alt="..."/>
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    )}
+};
+
+export default connect((state) => ({
+  credentials: state.credentials,
+  infoUser: state.infoUser,
+}))(Admin);
