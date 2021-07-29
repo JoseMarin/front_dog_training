97c553ac0c5c40e2c0137a6358aeba5d783f94b8
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-07-29 18:29:32 +0200

    Center the img

--- src/containers/Profile/Profile.jsx
+++ src/containers/Profile/Profile.jsx
@@ -3,6 +3,8 @@ import { useHistory, Link } from "react-router-dom";
 import { connect } from "react-redux";
 // import spinner from "../../assets/spinner2.gif";
 import moment from "moment";
+import Pensando from '../../assets/Buenos_modales/perro-pensando.png';
+
 
 const Profile = (props) => {
   let history = useHistory();
@@ -10,22 +12,26 @@ const Profile = (props) => {
   if (props.credentials?.token) {
     return (
       <div
-        class="card  mb-3 rounded mx-auto d-block  card"
+        class="card  mb-3 rounded mx-auto d-block border-0 card"
         Style="max-width: 540px;"
       >
         <div class="row g-0">
-          <div class="col-md-4">
-            <img src="..." class="img-fluid rounded-start" alt="..." />
+          <div class="col-md-4 m-auto">
+            <img src={Pensando} class="img-fluid rounded-circle  z-depth-2" alt="100x100"/>
           </div>
           <div class="col-md-8">
             <div class="card-body">
               <h5 class="card-title text-center">
                 Welcome&nbsp; &nbsp; {props.credentials?.user.name}
               </h5>
-              <p class="card-text text-center">Email {props.credentials?.user.email} </p>
-              <p class="card-text text-center">City <div></div> {props.credentials?.user.city}</p>
               <p class="card-text text-center">
-                CREATECOUNT {" "}<div></div>
+                Email {props.credentials?.user.email}{" "}
+              </p>
+              <p class="card-text text-center">
+                City <div></div> {props.credentials?.user.city}
+              </p>
+              <p class="card-text text-center">
+                CREATECOUNT <div></div>
                 {moment(props.credentials?.user.createdAt).format("LL")}{" "}
                 {/*Con 3 LLL te muestra la hora*/}
               </p>
