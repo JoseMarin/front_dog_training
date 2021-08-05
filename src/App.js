72dc66cefd9d98835ec7cbf19e364594737c4f6f
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-08-05 11:21:10 +0200

    Import posts component

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import AllUsers from './containers/Admin/AllUsers/AllUsers.jsx';
 import Profile from './containers/Profile/Profile.jsx';
 import UpdateUser from './containers/Profile/UpdateUser';
 import CommonWall from './containers/Common/CommonWall.jsx';
+import Posts from './components/posts/Posts';
 
 
 function App() {
@@ -34,6 +35,7 @@ function App() {
           <Route path="/admin" exact component={Admin}/>
           <Route path="/allusers" exact component={AllUsers}/>
           <Route path="/profile" exact component={Profile}/>
+          <Route path="/posts" exact component={Posts}/>
           <Route path="/updateuser" exact component={UpdateUser}/>
           <Route path="/commonwall" exact component={CommonWall}/>
         </Switch>
