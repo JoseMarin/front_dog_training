ebd760cdd27596bb150415e76d9dc687b523c2e7
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-08-06 11:08:24 +0200

    Remove the body, && just send the token

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,26 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import axios from "axios";
+import { ADD_POST } from '../../redux/types';
 // import moment  from 'moment';
 
 const Posts = (props) => {
-
-  const [post, setPost] = useState({
-    title: "",
-    content: "",
-    image: "",
-    user: props.credentials?.user,
-    name: props.credentials?.user.name,
-    lastName: props.credentials?.user.lastName,
-    date: props.credentials?.date,
-    token: props.credentials?.token,
-  });
 
   const [userPost, setUserPost] = useState([]);
 
   useEffect(() => {
     findPost();
-    setPost();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -28,41 +17,29 @@ const Posts = (props) => {
 
     let token = props.credentials?.token;
 
-    let user = post.user;
-
-    let body = {
-      title: post.title,
-      content: post.content,
-      userName: post.name,
-      lastName : post.lastName,
-      date: post.date,
-      userId: user.id,
-    };
-
     axios
-      .get("http://localhost:5000/post", body, {
+      .get("http://localhost:5000/post",  {
         headers: { authorization: "Bearer " + token },
       })
       .then((res) => {
         setUserPost(res.data.results);
         console.log('posts', res);
-        console.log('estoy en el then');
-        // props.dispatch({ type: ADD_POST, payload: res.data });
+        props.dispatch({ type: ADD_POST, payload: res.data });
       })
       .catch((err) => {
         console.log("Err");
-        console.log(err.response.data);
+        // console.log(err.response.data);
       });
   };
 
   if (userPost?.id) {
     return (
-      <div>
+      <div className="return">
         <div className="card mjs" Style="width: 18rem;">
           {userPost.map((mjs, index) => (
             <div className="card-body" key={index}>
               <img className="card-img-top" src=".../100px180/" alt="100x100" />
-              <h5 className="card-title">{mjs.data.title}</h5>
+              <h5 className="card-title">{mjs.title}</h5>
               <p className="card-text">{mjs.data.content}</p>
               <p className="card-text">User &nbsp; &nbsp; {mjs.data.name}</p>
             </div>
