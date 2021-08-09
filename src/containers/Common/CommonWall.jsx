6ad0799025cc031a687065c0517ac1ce6f59c0f0
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-08-09 16:47:56 +0200

    Card posts created

--- src/containers/Common/CommonWall.jsx
+++ src/containers/Common/CommonWall.jsx
@@ -2,48 +2,59 @@
 import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import axios from "axios";
-// import moment  from 'moment';
+import { ADD_POST } from "../../redux/types";
+import moment from "moment";
 import MakePost from "../../components/MakePost/MakePost";
 
-const CommonWall = () => {
+const CommonWall = (props) => {
   const [userPost, setUserPost] = useState([]);
 
   useEffect(() => {
     findPost();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  });
+  }, []);
 
-const findPost = async () => {
-  axios
-    .get("http://localhost:5000/post")
-    .then((res) => {
-      setUserPost(res.data);
-      // props.dispatch({ type: ADD_POST, payload: res.data });
-      console.log("userPosts", res);
-    })
-    .catch((err) => {
-      console.log("Err");
-    });
-};
+  const findPost = async () => {
+    axios
+      .get("http://localhost:5000/post")
+      .then((res) => {
+        setUserPost(res.data);
+        props.dispatch({ type: ADD_POST, payload: res?.data });
+      })
+      .catch((err) => {
+        console.log("Err");
+      });
+  };
 
   return (
     <div>
       <MakePost />
       {userPost ? (
         <div className="container">
-          <div className="card mjs mt-3">
-            {userPost.map((mjs, index) => (
-              <div className="card-body" key={index}>
-                <img
-                  className="card-img-top"
-                  src=".../100px180/"
-                  alt="100x100"
-                />
-                <h5 className="card-title">{mjs.title}</h5>
-                <p className="card-text">{mjs.content}</p>
-                <p className="card-text">User &nbsp; &nbsp; {mjs.name}</p>
+          <div className="card-group mt-3">
+            <div className="card">
+              <div className="">
+                {userPost.map((mjs, index) => (
+                  <div className="card-body" key={index}>
+                    <img
+                      className="card-img-top"
+                      src=".../100px180/"
+                      alt="100x100"
+                    />
+                    <h5 className="card-title">{mjs.title}</h5>
+                    <p className="card-text">{mjs.content}</p>
+                    {/* <p className="card-text">User &nbsp; &nbsp; {mjs.name}</p> */}
+                    <small class="text-muted">
+                      By &nbsp; {mjs.userName} &nbsp; {mjs.lastName}
+                    </small>{" "}
+                    &nbsp;
+                    <small class="text-muted">
+                      Date {moment(mjs.date).format("LLL")}
+                    </small>
+                  </div>
+                ))}
               </div>
-            ))}
+            </div>
           </div>
         </div>
       ) : (
