3a175a7329f2f95fd4e46a5ac24b963c4725552f
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-08-24 11:54:58 +0200

    Add the actions to create a post && find them

--- src/Actions/PostActions.jsx
+++ src/Actions/PostActions.jsx
@@ -1,97 +1,127 @@
 import {
-  ADD_POST_SUCCE, ADD_POST_ERROR,
+  ADD_POST,
+  ADD_POST_SUCCE,
+  ADD_POST_ERROR,
   GET_POST,
-  // REMOVE_POST,
+  GET_POST_SUCCE,
+  GET_POST_ERROR,
   // ADD_COMMENT,
-  // GET_POST_ERROR,
-  // REMOVE_POST_ERROR,
+  // REMOVE_POST,
   // REMOVE_POST_SUCCE,
-  // EDIT_POST_ERROR,
+  // REMOVE_POST_ERROR,
   // GET_POST_EDIT,
   // EDIT_POST_SUCCE,
+  // EDIT_POST_ERROR,
 } from "../redux/types";
 import axios from "axios";
-import { notification } from "antd";
+import Swal from "sweetalert2";
 
-//Create new products, and we call with useDispatch
+// Create new products, and we call with useDispatch
 export function createPostAction(body) {
   return async (dispatch) => {
-      dispatch( addPostSucce() );
-
-      axios
+    dispatch(addPost());
+    axios
       .post("http://localhost:5000/post", body, {
         headers: { authorization: "Bearer " },
       })
       .then((res) => {
-        // dispatch({ type: ADD_POST_SUCCE, payload: res?.data });
-        dispatch(addPostSucce(body)); //This is to state
+        dispatch(addPostSucce(body) ); //This is to state
         // window.location.reload();
-        notification.success({
-          message: "Post was add.",
-          style: { top: 76 },
-          description: "Post was add.",
-        });
+        //Alert
+        Swal.fire("Correct", "Post was add.", "success");
         // history.push("/commonwall");
       })
       .catch((err) => {
-        console.log("Err");
-        // props.dispatch(addPostError(true));
-        // dispatch({ type: ADD_POST_ERROR, payload: err?.data });
+        console.log(err);
         //But if there is an error, change the state
-        dispatch(addPostError(true));
+        dispatch(addPostError(true) );
         //Alert error
-        notification.error({
-          message: "Error",
-          style: { top: 76 },
-          description: "Action canceled.",
+        Swal.fire({
+          icon: "error",
+          title: "Was a mistake",
+          text: "Was a mistake try again.",
+          // getPostAction: getPostAction()
         });
-        // console.log(err.response.data);
       });
   };
 }
 
+const addPost = () => ({
+  type: ADD_POST,
+  payload: true,
+});
 //If the product is saved in the database and modificate the state
-const addPostSucce = (post) => ({
+const addPostSucce = body => ({
   type: ADD_POST_SUCCE,
-  payload: post,
+  payload: body,
 });
 
 //If there was an error. state take the state of error
-const addPostError = (state) => ({
+const addPostError = state => ({
   type: ADD_POST_ERROR,
   payload: state,
 });
-// Function to download the products from the data base
-// export function getProductsAction() {
-//   //Note getProductsAction, run the function wownloadProducts
-//   return async (dispatch) => {
-//     dispatch(downloadProducts());
 
-//     try {
-//       const aswer = await clientAxios.get("/products");
-//       dispatch(downloadProductsSucce(aswer.data)); //Put dispatch if the call is succe
-//     } catch (error) {
-//       console.log(error);
-//       dispatch(downloadProductsError());
-//     }
-//   };
-// }
+// Function to download the products from the data base
+export function getPostAction() {
+  //Note getPostAction, run the function wownloadProducts
+  return async (dispatch) => {
+    dispatch(downloadPost());
+
+    await axios
+      .post("http://localhost:5000/post", {
+        headers: { authorization: "Bearer " },
+      })
+      .then((res) => {
+        dispatch(downloadPostSucce()); //Put dispatch if the call is succe
+        // window.location.reload();
+        // history.push("/commonwall");
+      })
+      .catch((err) => {
+        console.log(err);
+        //But if there is an error, change the state
+        dispatch(downloadPostError(true));
+        //Alert error
+        Swal.fire({
+          icon: "error",
+          title: "Was a mistake",
+          text: "Was a mistake try again.",
+        });
+      });
+  };
+}
+
+const downloadPost = () => ({
+  type: GET_POST,
+  payload: true,
+});
+
+const downloadPostSucce = () => ({
+  type: GET_POST_SUCCE,
+  payload: true
+});
+
+const downloadPostError = () => ({
+  type: GET_POST_ERROR,
+  payload: true
+});
+
 
 // actions
-export const findPost = (props) => async (dispatch, getState) => {
-  let token = props.credentials?.token;
-  axios
-    .get("http://localhost:5000/post", {
-      headers: { authorization: "Bearer " + token },
-    })
-    .then((res) => {
-      dispatch({
-        type: GET_POST,
-        payload: res?.data,
-      });
-    })
-    .catch((err) => {
-      // notification.warning({message:'Atencion.',description: JSON.stringify(err.response.data.message)});
-      console.log("Err");
-    });
-};
+// export const findPost = (props) => async (dispatch, getState) => {
+//   let token = props.credentials?.token;
+//   axios
+//     .get("http://localhost:5000/post", {
+//       headers: { authorization: "Bearer " + token },
+//     })
+//     .then((res) => {
+//       dispatch({
+//         type: GET_POST,
+//         payload: res?.data,
+//       });
+//     })
+//     .catch((err) => {
+//       // notification.warning({message:'Atencion.',description: JSON.stringify(err.response.data.message)});
+//       console.log("Err");
+//     });
+// };
