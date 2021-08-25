550aeb21a9f4727128cab7a5800772656accabea
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-08-25 14:12:57 +0200

    Add function to editPost

--- src/Actions/PostActions.jsx
+++ src/Actions/PostActions.jsx
@@ -9,29 +9,30 @@ import {
   REMOVE_POST_SUCCE,
   REMOVE_POST_ERROR,
   GET_POST_EDIT,
+  START_EDIT_POST,
   EDIT_POST_SUCCE,
   EDIT_POST_ERROR,
-  // ADD_COMMENT,
+  ADD_COMMENT,
 } from "../redux/types";
 import axios from "axios";
 import Swal from "sweetalert2";
 
 export function createPostAction(body) {
   return async (dispatch) => {
-    dispatch(addPost() );
+    dispatch(addPost());
     await axios
       .post("http://localhost:5000/post", body, {
         headers: { authorization: "Bearer " },
       })
       .then((res) => {
-        dispatch(addPostSucce(body) ); //This is to state
+        dispatch(addPostSucce(body)); //This is to state
         //Alert
         Swal.fire("Correct", "The post was added successfully.", "success");
       })
       .catch((err) => {
         console.log(err);
         //But if there is an error, change the state
-        dispatch(addPostError(true) );
+        dispatch(addPostError(true));
         //Alert error
         Swal.fire({
           icon: "error",
@@ -47,13 +48,13 @@ const addPost = () => ({
   payload: true,
 });
 //If the product is saved in the database and modificate the state
-const addPostSucce = body => ({
+const addPostSucce = (body) => ({
   type: ADD_POST_SUCCE,
   payload: body,
 });
 
 //If there was an error. state take the state of error
-const addPostError = state => ({
+const addPostError = (state) => ({
   type: ADD_POST_ERROR,
   payload: state,
 });
@@ -62,19 +63,19 @@ export function getPostAction(props) {
   let token = props;
   //Note getPostAction, run the function wownloadProducts
   return async (dispatch) => {
-    dispatch(downloadPost() );
+    dispatch(downloadPost());
 
     await axios
       .get("http://localhost:5000/post", {
         headers: { authorization: "Bearer " + token },
       })
       .then((res) => {
-        dispatch(downloadPostSucce(res.data) ); //Put dispatch if the call is succe
+        dispatch(downloadPostSucce(res.data)); //Put dispatch if the call is succe
       })
       .catch((err) => {
         console.log(err);
         //But if there is an error, change the state
-        dispatch(downloadPostError() );
+        dispatch(downloadPostError());
         //Alert error
         Swal.fire({
           icon: "error",
@@ -90,7 +91,7 @@ const downloadPost = () => ({
   payload: true,
 });
 
-const downloadPostSucce = Userposts => ({
+const downloadPostSucce = (Userposts) => ({
   type: GET_POST_SUCCE,
   payload: Userposts,
 });
@@ -103,31 +104,31 @@ const downloadPostError = () => ({
 export function removePostAction(postId, userId) {
   return async (dispatch) => {
     //Here we collect the identifiers
-    dispatch(removePost(postId, userId) );
+    dispatch(removePost(postId, userId));
 
     await axios
       .put("http://localhost:5000/post/deletepost", { postId, userId })
       .then((res) => {
         //If it is eliminated show alert
-        dispatch(removeSucce(postId, userId) );
+        dispatch(removeSucce(postId, userId));
         Swal.fire("Deleted!", "Your post has been deleted.", "success");
       })
       .catch((err) => {
         // console.log(err.response.data);
         console.log(err);
-        dispatch(removeError() );
+        dispatch(removeError());
       });
   };
 }
 
 const removePost = (postId, userId) => ({
   type: REMOVE_POST,
-  payload: ({postId, userId})
+  payload: { postId, userId },
 });
 
 const removeSucce = (postId, userId) => ({
   type: REMOVE_POST_SUCCE,
-  payload: ({postId, userId})
+  payload: { postId, userId },
 });
 
 const removeError = () => ({
@@ -135,13 +136,47 @@ const removeError = () => ({
   payload: true,
 });
 
+//Put product on edition
 export function editPost(post) {
   return (dispatch) => {
-    dispatch(getPostRemove(post) )
+    dispatch(getPostRemove(post));
+  };
+}
+
+const getPostRemove = (post) => ({
+  type: GET_POST_EDIT,
+  payload: post,
+});
+
+//Edit a record in the API and status
+export function editPostAction(post, props) {
+  let token = props;
+  return async (dispatch) => {
+    dispatch(startEdit(post) );
+    await axios
+    .put("http://localhost:5000/post/updatepost", post, {
+      headers: { authorization: "Bearer " + token },
+    })
+    .then((res) => {
+        console.log('put-->', res);
+        dispatch(downloadPostSucce(res.data)); //Put dispatch if the call is succe
+      })
+      .catch((err) => {
+        // console.log(err);
+        console.log(err.response.data);
+        //But if there is an error, change the state
+        dispatch(downloadPostError());
+        //Alert error
+        Swal.fire({
+          icon: "error",
+          title: "Was a mistake",
+          text: "Try again.",
+        });
+      });
   }
 }
 
-const getPostRemove = post => ({
-  type: GET_POST_EDIT,
+const startEdit = post => ({
+  type: START_EDIT_POST,
   payload: post
 });
