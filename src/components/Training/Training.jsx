ba4e4315dd902480141deae7f433fc2fed78e38a
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-08-25 02:18:03 +0200

    Update the browser, all the links

--- src/components/Training/Training.jsx
+++ src/components/Training/Training.jsx
@@ -4,33 +4,13 @@ import { connect } from "react-redux";
 import { useHistory } from "react-router";
 
 const Training = (props) => {
-
   let history = useHistory();
 
-  // const [, setComedy] = useState([]);
+  if (props.credentials?.token) {
+  }
 
-//   const change = async () => {
-//     let genre = document.getElementById("opciones").value;
-
-//     axios
-//       .get(
-//         `https://api.themoviedb.org/3/discover/movie?api_key=79a61f5dc13e3e9e4834fadbf4f326c7&language=en-US&with_genres=${genre}`
-//       )
-//       .then((res) => {
-//         setComedy(res.data.results);
-
-//         // history.push("/moviesgenre");
-//       })
-//       .catch(() => {
-//         throw new Error("Wrong user or password");
-//     });
-// };
-
-    if (props.credentials?.token) {
-    }
-
-    return (
-        <div class="dropdown training">
+  return (
+    <div class="dropdown training">
       <ul class="navbar-nav">
         <li class="nav-item dropdown" id="dmenu">
           <div
@@ -44,26 +24,38 @@ const Training = (props) => {
             TRAINING
           </div>
           <ul
-            class="dropdown-menu dropdown-menu-dark"
+            class="dropdown-menu text-center dropdown-menu-dark"
             aria-labelledby="navbarDarkDropdownMenuLink"
           >
             <li>
-              <div class="dropdown-item" onClick={() => history.push("/buenosmodales")}>
+              <div
+                class="dropdown-item"
+                onClick={() => history.push("/buenosmodales")}
+              >
                 Buenos Modales
               </div>
             </li>
             <li>
-              <div class="dropdown-item" onClick={() => history.push("/obedienciabasica")}>
+              <div
+                class="dropdown-item"
+                onClick={() => history.push("/obedienciabasica")}
+              >
                 Obediencia Básica
               </div>
             </li>
             <li>
-              <div class="dropdown-item" onClick={() => history.push("/ejercicios")}>
+              <div
+                class="dropdown-item"
+                onClick={() => history.push("/ejercicios")}
+              >
                 Ejercicios
               </div>
             </li>
             <li>
-              <div class="dropdown-item" onClick={() => history.push("/perronuevo")}>
+              <div
+                class="dropdown-item"
+                onClick={() => history.push("/perronuevo")}
+              >
                 Perro Nuevo
               </div>
             </li>
